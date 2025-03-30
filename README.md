05f43f919216e60cb19a21a00aa4136b337f383a
Guzman Canivares Enzo Nicolas <nicocanivares2002@gmail.com>
2025-03-29 23:59:11 -0300

    Commit del punto 3f

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/kl-E8VQf)
+
+3.c) En mi repositorio estan los archivos: README.md, tp1_1.c, tp1_1.exe.
+No hace falta que esté el archivo .exe
--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+
 int main()
 {
-    
-    printf("Hola mundo");
+    int variable = 21;
+    int *punt = &variable;
+    printf("\nHola Mundo");
+    printf("\nContenido del puntero: %d",*punt);
+    printf("\nDireccion de memoria almacenada por el puntero: %p", punt);
+    printf("\nDireccion de memoria de la variable: %p", &variable);
+    printf("\nDireccion de memoria del puntero: %p",&punt);
+    printf("\nTamaño de memoria utilizado por la variable %d bytes", sizeof(variable));
     return 0;
 }
